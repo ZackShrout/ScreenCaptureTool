4ed7ccffc429ccc8109fe994b4e51f23df6eb4df
ZackShrout <zshrout@gmail.com>
2021-05-24 22:04:15 -0500

    Hideapplication for 5 seconds once capture button pressed, added a save button that is enabled once you capture a .bmp file

--- ScreenCaptureTool/Form1.Designer.cs
+++ ScreenCaptureTool/Form1.Designer.cs
@@ -30,23 +30,35 @@ namespace ScreenCaptureTool
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(388, 292);
+            this.button1.Location = new System.Drawing.Point(12, 415);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Capture";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(93, 415);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveButton);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@ namespace ScreenCaptureTool
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
--- ScreenCaptureTool/Form1.cs
+++ ScreenCaptureTool/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,13 +18,22 @@ namespace ScreenCaptureTool
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void GrabScreen()
         {
             Rectangle rect = Screen.PrimaryScreen.Bounds; // primary screen bounds... in case of multiple screens
             Bitmap bitmap = new Bitmap(rect.Width, rect.Height);
             Graphics graphic = Graphics.FromImage(bitmap);
             graphic.CopyFromScreen(0, 0, 0, 0, rect.Size);
             this.BackgroundImage = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Thread.Sleep(5000);
+            GrabScreen();
+            this.Show();
+            saveButton.Enabled = true;
         }
     }
 }
